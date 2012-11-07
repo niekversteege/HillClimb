1908a4056f2d473e77fccda034c901a6f4434f85
Niek <niekversteege@gmail.com>
2012-11-07 02:17:28 +0100

    made default field a bit better

--- src/main/java/nl/niek/minor/aa/hillclimb/field/FieldFactory.java
+++ src/main/java/nl/niek/minor/aa/hillclimb/field/FieldFactory.java
@@ -21,11 +21,11 @@ public class FieldFactory
 		int[] row2 = new int[] { 8, 3, 9, 4, 2, 1, 1, 4, 3, 2 };
 		int[] row3 = new int[] { 2, 4, 5, 1, 4, 3, 4, 3, 7, 9 };
 		int[] row4 = new int[] { 1, 9, 2, 6, 2, 9, 3, 5, 7, 1 };
-		int[] row5 = new int[] { 1, 9, 2, 6, 2, 9, 3, 5, 7, 3 };
-		int[] row6 = new int[] { 1, 9, 2, 6, 2, 9, 3, 5, 7, 5 };
-		int[] row7 = new int[] { 1, 9, 2, 6, 2, 9, 3, 5, 7, 3 };
-		int[] row8 = new int[] { 1, 9, 2, 6, 2, 9, 3, 5, 7, 6 };
-		int[] row9 = new int[] { 1, 9, 2, 6, 2, 9, 3, 5, 7, 0 };
+		int[] row5 = new int[] { 1, 9, 2, 6, 6, 9, 3, 5, 4, 3 };
+		int[] row6 = new int[] { 1, 7, 2, 6, 2, 9, 4, 5, 7, 5 };
+		int[] row7 = new int[] { 1, 9, 2, 6, 7, 6, 1, 9, 2, 7 };
+		int[] row8 = new int[] { 1, 7, 2, 6, 2, 7, 3, 5, 7, 7 };
+		int[] row9 = new int[] { 1, 2, 3, 4, 2, 9, 3, 5, 4, 0 };
 
 		field.addRow(row0);
 		field.addRow(row1);
